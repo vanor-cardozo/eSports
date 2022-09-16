e0579af18207342bd0e68cf48d7b6a6c243cb5b1
Vanor Cardozo <vanor.fc@gmail.com>
2022-09-16 16:50:38 -0300

    feat: tela Home

---
+++ mobile/src/screens/Home/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import { View, Image, Text } from 'react-native';
+
+import logoImg from '../../assets/logo-nlw-esports.png'
+import { Heading } from '../../components/Heading';
+
+import { styles } from './styles';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Image
+        source={logoImg}
+        style={styles.logo}
+      />
+
+      <Heading
+        title="Encontre seu duo!"
+        subtitle="Selecione o game que deseja jogar..."
+      />
+    </View>
+  );
+}
