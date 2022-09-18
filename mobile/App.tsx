ab389d6c7ff5847b1d948ac94b7a2915f8dc4421
Vanor Cardozo <vanor.fc@gmail.com>
2022-09-18 18:06:29 -0300

    feat: cria rotas para home e game

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,8 @@
 import { Background } from './src/components/Background';
 import { StatusBar } from 'react-native';
 import { useFonts, Inter_400Regular, Inter_600SemiBold, Inter_700Bold, Inter_900Black } from '@expo-google-fonts/inter';
-import { Home } from './src/screens/Home';
 import { Loading } from './src/components/Loading';
+import { Routes } from './src/routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -19,7 +19,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Home /> : <Loading/>}
+      {fontsLoaded ? <Routes/> : <Loading/>}
     </Background>
   );
 }
