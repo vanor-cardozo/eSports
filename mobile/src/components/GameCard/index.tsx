df753236a3d32ecc83cf8c1b82a5613c2e9b6c2e
Vanor Cardozo <vanor.fc@gmail.com>
2022-09-18 18:07:48 -0300

    feat: componente para Card do jogo

--- mobile/src/components/GameCard/index.tsx
+++ mobile/src/components/GameCard/index.tsx
@@ -1,13 +1,15 @@
-import { TouchableOpacity, TouchableOpacityProps, ImageBackground, ImageSourcePropType, Text } from 'react-native';
+import { TouchableOpacity, TouchableOpacityProps, ImageBackground, Text } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
 import { styles } from './styles';
 import { THEME } from '../../theme';
 
 export interface GameCardProps {
     id: string;
-    name: String;
-    ads: String;
-    cover: ImageSourcePropType;
+    title: string;
+    _count: {
+        ads: number
+    };
+    bannerUrl: string;
 }
 
 interface Props extends TouchableOpacityProps {
@@ -16,20 +18,20 @@ interface Props extends TouchableOpacityProps {
 
 export function GameCard({ data, ...rest }: Props) {
   return (
-    <TouchableOpacity style={styles.container}>
+    <TouchableOpacity style={styles.container} {...rest}>
         <ImageBackground
             style={styles.cover}
-            source={data.cover}
+            source={{ uri: data.bannerUrl }}
         >
             <LinearGradient
                 colors={THEME.COLORS.FOOTER}
                 style={styles.footer}
             >
                 <Text style={styles.name}>
-                    {data.name}
+                    {data.title}
                 </Text>
                 <Text style={styles.ads}>
-                    {data.ads} anúncios
+                    {data._count.ads} anúncios
                 </Text>
             </LinearGradient>
         </ImageBackground>
